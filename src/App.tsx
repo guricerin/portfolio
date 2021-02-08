b448d9568380a10f3931e90245007639d590e339
guricerin <chanbo1e9@gmail.com>
2021-02-08 22:47:00 +0900

    to typesctipt

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './logo.svg';
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <p>hoge fuga</p>
+      <img src={logo}></img>
+    </div>
+  )
+}
+
+export default App;
